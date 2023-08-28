6678edfc3bfd493efafc63bded6e4e7aed7f04f5
Mary McKinnon <mary.mckinnon@health-union.com>
2023-08-27 23:07:04 -0700

    Native hooks exploration

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { ScrollView, Text, StyleSheet, Image, View } from 'react-native';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  Image,
+  View,
+  useColorScheme,
+  // useWindowDimensions
+} from 'react-native';
 
 export default function WelcomeScreen() {
+  const colorScheme = useColorScheme();
+  // const window = useWindowDimensions();
   return (
-    <ScrollView style={welcomeStyles.container}>
+    <ScrollView style={[
+      welcomeStyles.container,
+      colorScheme === 'light' ? { backgroundColor: 'white' } : { backgroundColor: 'black' },
+    ]}>
       <View style={welcomeStyles.headerWrapper}>
         <Image
-          style={welcomeStyles.logo}
+          style={welcomeStyles.image}
           source={require('../img/littleLemonAppLogo.png')}
           resizeMode={'cover'}
           accessible={true}
           accessibilityLabel={'Little Lemon Logo'}
         />
-        <Text style={welcomeStyles.headerText}>Little Lemon</Text>
+        <Text style={[
+          welcomeStyles.headerText,
+          colorScheme === 'light'
+              ? { color: '#333333' }
+              : { color: '#EDEFEE' },
+        ]}>Little Lemon</Text>
       </View>
-      <Text style={welcomeStyles.title}>
+      <Text style={[
+        welcomeStyles.headerText,
+        colorScheme === 'light' ? { color: '#333333' } : { color: '#EDEFEE' },
+      ]}>
         Little Lemon is a charming neighborhood bistro that serves simple
         food and classic cocktails in a lively but casual environment. We
         would love to hear your experience with us!
       </Text>
+      {/* <Text>Window Dimensions</Text>
+      <Text>Height: {window.height}</Text>
+      <Text>Width: {window.width}</Text>
+      <Text>Font scale: {window.fontScale}</Text> */}
       {/* <Image
         style={welcomeStyles.image}
         source={require('../img/image1.png')}
@@ -54,9 +79,11 @@ export default function WelcomeScreen() {
 const welcomeStyles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    marginTop: 25,
-    backgroundColor: '#EDEFEE',
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
   },
   headerText: {
     paddingRight: 10,
@@ -64,45 +91,19 @@ const welcomeStyles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 10,
     fontSize: 30,
-    color: 'black',
+    color: '#EDEFEE',
     textAlign: 'center',
   },
-  headerWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    margin: 10,
-  },
-  mainText: {
+  regularText: {
     fontSize: 24,
     padding: 20,
     marginVertical: 8,
     color: '#EDEFEE',
     textAlign: 'center',
   },
-  inputBox: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    borderColor: '#EDEFEE',
-    backgroundColor: '#EDEFEE',
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 10,
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333333',
-  },
   image: {
     width: 100,
     height: 100,
     borderRadius: 20,
-  },
-  logo: {
-    width: 75,
-    height: 75,
-  },
+  }
 });
